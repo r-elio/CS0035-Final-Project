10914a9cc721cb3a9d19ee61c3d5efc0f90360a2
Rajan Elio <57312970+r-elio@users.noreply.github.com>
2021-10-24 15:14:07 +0800

    added the java implementation for ATM System

---
+++ Java/atm/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package Java.atm;
+
+public class InvalidPasswordException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage() {
+		return "InvalidPasswordException:\n";
+	}
+}
