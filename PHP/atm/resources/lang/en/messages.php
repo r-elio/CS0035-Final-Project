47492dc19685943723a360884d72be1811527dc4
Rajan Elio <57312970+r-elio@users.noreply.github.com>
2021-10-28 04:42:22 +0800

    remove the info of other features

--- PHP/atm/resources/lang/en/messages.php
+++ PHP/atm/resources/lang/en/messages.php
@@ -7,7 +7,7 @@ return [
     'about' => 'About',
     'about atm' => 'About ATM',
     'about us' => 'About Us',
-    'about atm content' => "Automated Teller Machine\nAdmin:\n-Add/Remove/Update Accounts\n-Import/Export/Append Accounts\n",
+    'about atm content' => "Automated Teller Machine\nAdmin:\n-Add/Remove/Update Accounts\n",
     'about us content' => "Created by:\nRajan Elio, Mark Egana,\nJames De Leon, & Agerico Reyes\nBSCSSE - TN41\n",
     'language' => 'Language',
     'english' => 'English',
